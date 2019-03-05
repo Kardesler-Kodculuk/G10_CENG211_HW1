209baf4ec8dba4c9e9f0092e8c05e022ca6f5fbd
ambertide <egeemirozkan24@gmail.com>
2019-03-05 15:01:25 +0300

    Add Item class

--- src/Item.java
+++ src/Item.java
@@ -1,10 +1,30 @@
 
 public class Item {
-	public String toString() {
-		return "Lol.";
+	private String name;
+	private String disposableType;
+	
+	/** Construct an Item. */
+	public Item(String name, String disposableType) {
+		this.name = name;
+		this.disposableType = disposableType;
+	}
+
+	/** Return the disposable type.*/
+	public String getDisposibleType() {
+		return this.disposableType;
 	}
 	
+	/** Returns string representation. */
+	public String toString() {
+		return this.name;
+	}
+	
+	/** Implements the equals method for Item class. */
 	public boolean equals(Object obj) {
-		return true;
+		if (obj == null) return false;
+		if (!obj.getClass().equals(this.getClass())) return false;
+		Item comperessantItem = (Item) obj;
+		return this.name.equals(comperessantItem.name) && 
+				this.disposableType.equals(comperessantItem.disposableType);
 	}
 }
--- src/PicnicBagApp.java
+++ src/PicnicBagApp.java
@@ -3,7 +3,12 @@ public class PicnicBagApp {
 
 	public static void main(String[] args) {
 		// Conduct Testing Here.
-
+		Item orange = new Item("Orange", "Plastic");
+		Item notOrange = new Item("Not Orange", "Plastic");
+		System.out.println(orange);
+		System.out.println(orange.equals(notOrange));
+		System.out.println(orange == notOrange);
+		System.out.println(orange.getDisposibleType());
 	}
 
 }
